110adb38d92d227a87febf29954acde6998b7cb2
Xueli-Zhang <xueli.zhang@carleton.ca>
2018-03-20 14:41:41 -0400

    all done except multiple layers

--- feed_forward_network.py
+++ feed_forward_network.py
@@ -18,7 +18,7 @@ class feed_forward_network:
 		self.outputWeights = 0.5; 
 
 		#activatoin functions or sigmoid
-		self.hidActivation = zeros((self.numOfHiddenNeuron, 1), dtype = float)
+		self.hidActivation = zeros((self.numOfHiddenNeuron, self.numOfHiddenLayer), dtype = float)
 		self.outputActivation = zeros((self.numOfOutput, 1), dtype = float)
 
 
@@ -31,15 +31,46 @@ class feed_forward_network:
 		self.hiddDelta = zeros((self.numOfHiddenNeuron), dtype = float)
 		self.outputDelta = zeros((self.numOfOutput), dtype = float)
 
+
 	def feed_forward(self, input):
+		#set input as the first layer of output and bias = 1
+		self.inputNeurons[:-1, 0] = input
+		self.inputNeurons[-1: 0] = 1.0
+
+		#hidden layer
+		self.hidActivation = self.sigmoid(self.hidWeights, self.inputNeurons)
+		self.hiddNeurons = self.output(self.hidActivation)
+
+		#set bias in hidden layer to 1
+		self.hiddNeurons[-1:,:] = 1.0
+
+		#output layer
+		self.outputActivation = self.sigmoid(self.outputWeights, self.hiddNeurons)
+		self.outputNeurons = self.output(self.outputActivation)
+
+	def backpropagation(self, result):
+		error = self.outputNeurons - array(result, dtype = float)
+
+		#delta of output neurons
+		self.outputDelta = (1 - tanh(self.outputActivation)) * tanh(self.outputActivation) * error
+
+		#delta of hidden layers
 
 
-	def sigmoid(self, input):
+		#aply weight changes
+		#hidden layers
+
+		#outputlayers
+		self.outputWeights = self.outputWeights - self.epsilon * dot(self.outputDelta, self.hiddNeurons[:-1].transpose())
+
+
+	def get_output(self):
+		return self.outputNeurons
+
+
+	def sigmoid(self, weights, output):
+
+	def output(self, activation):
 
 	def sum(self, input):
-
-	def back_propergation(self, train):
-
-	def output(self):
-		return self.outputNeurons
 
